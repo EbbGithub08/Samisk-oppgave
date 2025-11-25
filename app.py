19ea73778f094a5454b3085b3a283b90ab52985e
EbbGithub08 <ebbe.gaston.zelow@gmail.com>
2025-11-25 13:11:08 +0100

    innit 2

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
 
 
 def create_app() -> Flask:
-    """Application factory for easier testing and configuration."""
     app = Flask(__name__)
 
     @app.get("/")
     def index():
-        return jsonify(message="Hello, Sápmi!"), 200
+        return render_template("index.html")
 
     return app
 
 
 if __name__ == "__main__":
     create_app().run(debug=True)
+    
 
+python -m pip install flask
