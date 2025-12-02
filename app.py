b612e93573a06ffe312ef18f33a05ec3c880618d
EbbGithub08 <ebbe.gaston.zelow@gmail.com>
2025-12-02 09:44:23 +0100

    fungerende linker

--- app.py
+++ app.py
@@ -8,8 +8,6 @@ def create_app() -> Flask:
     def index():
         return render_template("index.html")
 
-    return app
-
     @app.get("/historie")
     def historie():
         return render_template("historie.html")
@@ -21,6 +19,8 @@ def create_app() -> Flask:
     @app.get("/kontakt")
     def kontakt():
         return render_template("kontakt.html")
+
+    return app
 
 if __name__ == "__main__":
     create_app().run(debug=True)
